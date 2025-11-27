e4250fa16df4f7ad0072242a2d8ffa2c577d39a5
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2025-11-27 16:28:16 +0900

    10

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,5 +1,6 @@
 package com.back;
 
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,15 +13,29 @@ public class AppConfig {
         return 55;
     }
 
-//    @Bean
-//    @Order(2)
-//    public ApplicationRunner applicationRunner(){
-//        return new MyApplicationRunner();
-//    }
-//
-//    @Bean
-//    @Order(1)
-//    public ApplicationRunner applicationRunner2(){
-//        return new MyApplicationRunner2();
-//    }
+    @Bean
+    ApplicationRunner myApplicationRunner() {
+        return new ApplicationRunner() {
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                System.out.println("myApplicationRunner is running");
+            }
+        };
+    }
+
+    @Bean
+    ApplicationRunner myApplicationRunner2() {
+        return args -> {
+            work1();
+            work2();
+        };
+    }
+
+    private void work1() {
+        System.out.println("work1");
+    }
+
+    private void work2() {
+        System.out.println("work2");
+    }
 }
