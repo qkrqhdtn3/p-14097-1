8a8ef076aed41e5c0ddaaa62cd75a26fa1a146c2
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2025-11-27 16:29:44 +0900

    11

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,13 +1,19 @@
 package com.back;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.core.annotation.Order;
 
 @Configuration
 public class AppConfig {
+    @Autowired
+    @Lazy
+    private AppConfig self;
+
     @Bean
     int version(){
         return 55;
@@ -26,8 +32,8 @@ public class AppConfig {
     @Bean
     ApplicationRunner myApplicationRunner2() {
         return args -> {
-            work1();
-            work2();
+            self.work1();
+            self.work2();
         };
     }
 
