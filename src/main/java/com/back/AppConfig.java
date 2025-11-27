cfd6d6828bfe8127fdd6b5052de0d37ed3c148cb
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2025-11-27 15:30:35 +0900

    07

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,5 +1,6 @@
 package com.back;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,5 +9,13 @@ public class AppConfig {
     @Bean
     int version(){
         return 55;
+    }
+
+    public ApplicationRunner applicationRunner(){
+        return new MyApplicationRunner();
+    }
+
+    public ApplicationRunner applicationRunner2(){
+        return new MyApplicationRunner();
     }
 }
