cde40462aadddace8f52705cd9a2444b21b69e4c
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2025-11-28 11:20:30 +0900

    32 feat: HtmlRenderer, Parser as beans

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -3,6 +3,8 @@ package com.back;
 import com.back.domain.member.member.entity.Member;
 import com.back.domain.member.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -19,6 +21,16 @@ public class AppConfig {
     @Autowired
     @Lazy
     private AppConfig self;
+
+    @Bean
+    HtmlRenderer htmlRenderer() {
+        return HtmlRenderer.builder().build();
+    }
+
+    @Bean
+    Parser parser() {
+        return Parser.builder().build();
+    }
 
     @Bean
     int version(){
