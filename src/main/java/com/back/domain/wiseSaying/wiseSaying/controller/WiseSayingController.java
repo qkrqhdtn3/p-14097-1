08d5a0f5f60f8bccea2306000ee1395f5147683a
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2025-11-28 09:37:28 +0900

    24 feat: wiseSaying 등록, 조회

---
+++ src/main/java/com/back/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
@@ -0,0 +1,50 @@
+package com.back.domain.wiseSaying.wiseSaying.controller;
+
+import com.back.domain.wiseSaying.wiseSaying.entity.WiseSaying;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+public class WiseSayingController {
+    private int lastId = 0;
+    private final List<WiseSaying> wiseSayings = new ArrayList<>();
+
+    @GetMapping("/wiseSayings/write")
+    @ResponseBody
+    public String wirte(
+            String content,
+            String author
+    ) {
+        if (content == null || content.isBlank()) {
+            throw new IllegalArgumentException("content cannot be null or blank");
+        }
+        if (author == null || author.isBlank()) {
+            throw new IllegalArgumentException("author cannot be null or blank");
+        }
+        int id = ++lastId;
+
+        WiseSaying wiseSaying = new WiseSaying(id, content, author);
+
+        wiseSayings.add(wiseSaying);
+
+        return "%d번 명언이 생성됐습니다.".formatted(id);
+    }
+
+    @GetMapping("/wiseSayings")
+    @ResponseBody
+    public String list() {
+        return "<ul>" +
+                wiseSayings
+                        .stream()
+                        .map(wiseSaying ->
+                                "<li>%d / %s / %s</li>".formatted(wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent())
+                        )
+                        .collect(Collectors.joining(""))
+                + "</ul>";
+    }
+}
